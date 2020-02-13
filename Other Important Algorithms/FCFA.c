6bf6a632e2af8fcfe1b646d38db223cb5f4a6e90
Gagan Agarwal <53825001+GaganAgarwal77@users.noreply.github.com>
2020-02-13 19:58:10 +0530

    Update FCFA.c

--- Other Important Algorithms/FCFA.c
+++ Other Important Algorithms/FCFA.c
@@ -1,3 +1,6 @@
+//Question : Check whether the given linked list is circular or not
+//Time Complexity : O(n)
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -11,16 +14,9 @@ struct Node
 
 void push(struct Node** head_ref, int new_data) 
 { 
-    /* 1. allocate node */
     struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
-   
-    /* 2. put in the data  */
     new_node->data  = new_data; 
-   
-    /* 3. Make next of new node as head */
     new_node->next = (*head_ref); 
-   
-    /* 4. move the head to point to the new node */
     (*head_ref)    = new_node; 
 } 
 
@@ -40,30 +36,7 @@ void makeitCircular(struct Node** head_ref,int n)
         }
         node ->next = point;
 }
-// void printList(struct Node *first) 
-// { 
-//     struct Node *temp = first;  
-  
-//     // If linked list is not empty 
-//     if (first != NULL)  
-//     { 
-//         // Keep printing nodes till we reach the first node again 
-//         do
-//         { 
-//             printf("%d ", temp->data); 
-//             temp = temp->next; 
-//         } 
-//         while (temp != first); 
-//     } 
-//} 
-// void printList(struct Node *node) 
-// { 
-//   while (node != NULL) 
-//   { 
-//      printf(" %d ", node->data); 
-//      node = node->next; 
-//   } 
-// }
+
 int checkIfCircular(struct Node** head_ref)
 {
     struct Node* p1 = malloc(sizeof(struct Node));
@@ -90,12 +63,9 @@ int main()
     int arr[] = {1,2,3,4,5,6,7,8,9,10};
     for(int i = 0;i<10;i++)
         push(&head,arr[i]);
-    //makeitCircular(&head,4);
     int ans;
     ans = checkIfCircular(&head);
     printf("%d\n",ans);
-    //printList(head);
-
 
     return 0;
 }
